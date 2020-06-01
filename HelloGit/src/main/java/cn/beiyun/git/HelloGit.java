5fae49a73b5e7d91e93450374326d3bf9c18c922
xiaozhang-pipi <1725505332@qq.com>
2020-06-01 15:13:06 +0800

    克隆更新

--- HelloGit/src/main/java/cn/beiyun/git/HelloGit.java
+++ HelloGit/src/main/java/cn/beiyun/git/HelloGit.java
@@ -10,5 +10,7 @@ public class HelloGit {
         System.out.println("主干更新2");
         System.out.println("主干更新3");
         System.out.println("在线更新");
+        System.out.println("克隆项目提交");
+
     }
 }
